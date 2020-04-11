3c05ea9ecb9165ba721d3f7ede14539dfb6c8274
moralesmiguel <51275356+moralesmiguel@users.noreply.github.com>
2020-04-11 10:46:05 -0500

    Adding route to follow and unfollow

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { userById, allUsers, getUser, updateUser, deleteUser, userPicture, hasAuthorization } = require("../controllers/user");
+const { userById, allUsers, getUser, updateUser, deleteUser, userPicture, hasAuthorization, addFollowing, addFollower, removeFollowing, removeFollower } = require("../controllers/user");
 const {requireSignin} = require("../controllers/auth");
 
 const router = express.Router();
 
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, hasAuthorization, updateUser);
